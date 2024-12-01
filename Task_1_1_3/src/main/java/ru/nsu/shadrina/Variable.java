31822ca75ecdb3fc03308060c471e662026e2663
Coilyun <nikitab4rodkin@yandex.ru>
2024-12-01 22:00:07 +0700

    Task_1_1_3

---
+++ Task_1_1_3/src/main/java/ru/nsu/shadrina/Variable.java
@@ -0,0 +1,31 @@
+package ru.nsu.shadrina;
+
+public class Variable extends Expression {
+    private final String name;
+
+    public Variable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void print() {
+        System.out.print(name);
+    }
+
+    @Override
+    public Expression derivative(String var) {
+        return new Number(name.equals(var) ? 1 : 0);
+    }
+
+    @Override
+    public int eval(String variables) {
+        String[] parts = variables.split(";");
+        for (String part : parts) {
+            String[] keyValue = part.split("=");
+            if (keyValue[0].trim().equals(name)) {
+                return Integer.parseInt(keyValue[1].trim());
+            }
+        }
+        return 0;
+    }
+}

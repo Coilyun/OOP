b72915e3a178409dfbc030c08a8eb32659f9fb99
Coilyun <Coilyun@users.noreply.github.com>
2024-12-01 15:15:10 +0000

    Deploying to gh-pages from @ Coilyun/OOP@030b3f91f94ebe8a5597149ab25393ecb2881263 🚀

---
+++ Task_1_1_3/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"ru.nsu.shadrina","l":"Add"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shadrina","l":"Expression"},{"p":"ru.nsu.shadrina","l":"Main"},{"p":"ru.nsu.shadrina","l":"Mul"},{"p":"ru.nsu.shadrina","l":"Number"},{"p":"ru.nsu.shadrina","l":"Variable"}]

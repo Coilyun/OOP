c6a42b013f00c51e6025f94857b1bd3bc9c77944
Coilyun <90952801+Coilyun@users.noreply.github.com>
2024-12-09 09:52:45 +0700

    Delete Task_1_1_3/src/main/java/ru/nsu/shadrina directory

--- Task_1_1_3/src/main/java/ru/nsu/shadrina/Expression.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.shadrina;
-
-public abstract class Expression {
-    public abstract void print();
-    public abstract Expression derivative(String var);
-    public abstract int eval(String variables);
-}
